3e4f1fef8af8b8a3bb3e968ad88ac851e5925259
Nathan Arthur <nathan@nathanarthur.com>
2020-07-09 15:13:37 -0400

    update pwa manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,11 +26,11 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `AudioVerse`,
+                short_name: `AudioVerse`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#be2200`,
+                theme_color: `#be2200`,
                 display: `minimal-ui`,
                 icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
             },
