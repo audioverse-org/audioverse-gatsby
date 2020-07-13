3389fca7de098156e50bd7b0edb12e737ad6c19a
Nathan Arthur <nathan@nathanarthur.com>
2020-07-13 12:13:26 -0500

    sermon style, redirects

--- src/templates/sermon.js
+++ src/templates/sermon.js
@@ -1,25 +1,28 @@
 import React from "react"
 import Layout from "../components/layout"
+import './sermon.css'
 
 export default function Sermon({ pageContext }) {
     const sermon = pageContext.node
     return (
         <Layout>
-            <h1>{sermon.title}</h1>
+            <div className={'template-sermon'}>
+                <h1>{sermon.title}</h1>
 
-            {sermon.recordingDate ? <p>{sermon.recordingDate}</p> : null}
+                <ul className={'template-sermon__speakers'}>
+                    {sermon.presenters.map(speaker => {
+                        return <li>{speaker.name}</li>
+                    })}
+                </ul>
 
-            <ul>
-                {sermon.presenters.map(speaker => {
-                    return <li>{speaker.name}</li>
+                {sermon.recordingDate ? <p>{(new Date(sermon.recordingDate)).toLocaleDateString()}</p> : null}
+
+                {sermon.description ? <div dangerouslySetInnerHTML={{__html: sermon.description}} /> : null}
+
+                {sermon.mediaFiles.map(file => {
+                    return <div><audio controls src={file.url}>Your browser doesn't support this player.</audio></div>
                 })}
-            </ul>
-
-            {sermon.description ? <div dangerouslySetInnerHTML={{__html: sermon.description}} /> : null}
-
-            {sermon.mediaFiles.map(file => {
-                return <div><audio controls src={file.url}>Your browser doesn't support this player.</audio></div>
-            })}
+            </div>
         </Layout>
     )
 }
