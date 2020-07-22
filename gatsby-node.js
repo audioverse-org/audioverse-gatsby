8ec007590020dd044954173bf729ff5219f23764
Nathan Arthur <nathan@nathanarthur.com>
2020-07-22 10:06:32 -0400

    Add sermons in all languages

--- gatsby-node.js
+++ gatsby-node.js
@@ -51,30 +51,42 @@ query {
     return await run()
 }
 
+const createSermon = async (createPage, node, pathPrefix) => {
+    await createPage({
+        path: `${pathPrefix}${node.id}`,
+        component: path.resolve(`./src/templates/sermon.js`),
+        context: {node}
+    })
+}
+
+const createLanguageSermons = async (graphql, createPage, pathPrefix, language) => {
+    const sermons = await getSermons(graphql, language)
+
+    if (!sermons) return;
+
+    await Promise.all(sermons.map(async (node) => {
+        await createSermon(createPage, node, pathPrefix)
+    }))
+}
+
 const createSermons = async (graphql, createPage) => {
-    const englishSermons = await getSermons(graphql)
-
-    if (englishSermons) {
-        englishSermons.forEach((node) => {
-            createPage({
-                path: `english/sermons/recordings/${node.id}`,
-                component: path.resolve(`./src/templates/sermon.js`),
-                context: {node}
-            })
-        })
+    const languages = {
+        'ENGLISH': 'english/sermons/recordings/',
+        'SPANISH': 'espanol/sermones/grabaciones/',
+        // TODO: Translate route:
+        'FRENCH': 'deutsch/sermons/recordings/',
+        'GERMAN': 'francais/predications/enregistrements/',
+        // TODO: Translate route:
+        'CHINESE': 'zhongwen/sermons/recordings/',
+        // TODO: Translate route:
+        'JAPANESE': 'ja/sermons/recordings/',
+        // TODO: Translate route:
+        'RUSSIAN': 'ru/sermons/recordings/',
     }
 
-    const spanishSermons = await getSermons(graphql, "SPANISH")
-
-    if (spanishSermons) {
-        spanishSermons.forEach((node) => {
-            createPage({
-                path: `espanol/sermones/grabaciones/${node.id}`,
-                component: path.resolve(`./src/templates/sermon.js`),
-                context: {node}
-            })
-        })
-    }
+    await Promise.all(Object.keys(languages).map(async (language) => {
+        await createLanguageSermons(graphql, createPage, languages[language], language)
+    }))
 }
 
 exports.createPages = async ({graphql, actions}) => {
