8b2213d1b3295a204adddee1dbec939914955c26
Nathan Arthur <nathan@nathanarthur.com>
2020-08-17 15:56:43 -0400

    Refactor sermon factory tests

--- src/templates/sermon.factory.spec.js
+++ src/templates/sermon.factory.spec.js
@@ -3,8 +3,20 @@ import {describe, expect, it, jest} from "@jest/globals";
 
 const factory = require('./sermon.factory.js')
 
-const testCreatePages = async (returnValue) => {
-    const graphql = jest.fn(() => Promise.resolve(returnValue)),
+const testCreatePages = async (nodes = []) => {
+    const returnValue = {
+            data: {
+                avorg: {
+                    sermons: {
+                        nodes,
+                        pageInfo: {
+                            hasNextPage: false
+                        }
+                    }
+                }
+            }
+        },
+        graphql = jest.fn(() => Promise.resolve(returnValue)),
         createPage = jest.fn();
 
     await factory.createPages(graphql, createPage)
@@ -14,52 +26,21 @@ const testCreatePages = async (returnValue) => {
 
 describe("sermon factory", () => {
     it("gets English sermons", async () => {
-        const {graphql} = await testCreatePages({
-            data: {
-                avorg: {
-                    sermons: {
-                        nodes: [],
-                        pageInfo: {
-                            hasNextPage: false
-                        }
-                    }
-                }
-            }
-        })
+        const {graphql} = await testCreatePages()
 
         expect(graphql.mock.calls[0][0]).toContain('ENGLISH')
     })
 
     it("creates English page", async () => {
-        const {createPage} = await testCreatePages({
-            data: {
-                avorg: {
-                    sermons: {
-                        nodes: [{}],
-                        pageInfo: {
-                            hasNextPage: false
-                        }
-                    }
-                }
-            }
-        })
+        const {createPage} = await testCreatePages([{}])
 
         expect(createPage.mock.calls[0][0].path).toContain('en/sermons/')
     })
 
     it("passes sermon data", async () => {
-        const {createPage} = await testCreatePages({
-            data: {
-                avorg: {
-                    sermons: {
-                        nodes: [{
-                            'title': 'the_title'
-                        }],
-                        pageInfo: {hasNextPage: false}
-                    }
-                }
-            }
-        })
+        const {createPage} = await testCreatePages([{
+            'title': 'the_title'
+        }])
 
         expect(createPage.mock.calls[0][0].context.node).toEqual({'title': 'the_title'})
     })
